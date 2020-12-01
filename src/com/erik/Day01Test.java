3849bd5931b360c3b04532c59744a2e7a20489c4
erikwastaken <46598911+erikwastaken@users.noreply.github.com>
2020-12-01 17:26:24 +0100

    Day 01

---
+++ src/com/erik/Day01Test.java
@@ -0,0 +1,19 @@
+package com.erik;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day01Test {
+    @Test
+    public void testPart1() {
+        Day01 day01 = new Day01();
+        assertEquals(514579, day01.part1("1721 979 366 299 675 1456"));
+    }
+
+    @Test
+    void testPart2() {
+        Day01 day01 = new Day01();
+        assertEquals(241861950, day01.part2("1721 979 366 299 675 1456"));
+    }
+}

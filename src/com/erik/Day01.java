56035d7672fd43916400c15512aa2a7907cf3244
erikwastaken <46598911+erikwastaken@users.noreply.github.com>
2020-12-01 17:26:24 +0100

    Day01

---
+++ src/com/erik/Day01.java
@@ -0,0 +1,43 @@
+package com.erik;
+
+import java.util.Arrays;
+
+public class Day01 {
+    public int part1(String input) {
+        int[] array = convertStringToSortedIntArray(input);
+
+        for (int i = 0; i<array.length - 1; i++) {
+            int toFind = 2020 - array[i];
+            int index = Arrays.binarySearch(array, toFind);
+            if (index>=0)
+                return array[i]*array[index];
+        }
+        return -1;
+    }
+
+    public int part2(String input) {
+        int[] array = convertStringToSortedIntArray(input);
+
+        for (int i = 0; i<array.length-2; i++) {
+            for (int j = i+1; j<array.length-1; j++) {
+                int toFind = 2020 - array[i] - array[j];
+                int index = Arrays.binarySearch(array,toFind);
+                if (index >= 0)
+                    return array[i]*array[j]*array[index];
+            }
+        }
+        return -1;
+    }
+
+    private int[] convertStringToSortedIntArray(String str) {
+        String[] stringArray = str.split(" ");
+        int[] intArray = new int[stringArray.length];
+
+        for (int i = 0; i<stringArray.length; i++) {
+            intArray[i] = Integer.parseInt(stringArray[i]);
+        }
+        Arrays.sort(intArray);
+
+        return intArray;
+    }
+}

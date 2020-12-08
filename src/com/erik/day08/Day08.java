5f609d8c2add569f0408be058eb6bb526128c4a6
erikwastaken <46598911+erikwastaken@users.noreply.github.com>
2020-12-08 16:14:08 +0100

    refactoring 1"

--- src/com/erik/day08/Day08.java
+++ src/com/erik/day08/Day08.java
@@ -40,28 +40,15 @@ public class Day08 {
 
     public int part2() {
         for (int i=0; i<input.length; i++) {
-            String currentStatement = "";
-            String command = input[i].split(" ")[0];
-            String value = input[i].split(" ")[1];
-            switch (command) {
-                case "jmp":
-                    currentStatement = "nop " + value;
-                    break;
-                case "nop":
-                    currentStatement = "jmp " + value;
-                    break;
-            }
+            String currentStatement = buildChangedStatement(input[i]);
             if (currentStatement.length() == 0)
                 continue;
 
-            String[] newInput = new String[input.length];
-            for (int k=0; k<newInput.length; k++)
-                newInput[k] = input[k];
+            String[] newInput = input.clone();
             newInput[i] = currentStatement;
 
             Set<Integer> seenLines = new HashSet<>();
-            this.counter = 0;
-            this.accumulator = 0;
+            resetCounters();
             while (seenLines.add(counter)) {
                 processInput(newInput[counter]);
                 if (counter == input.length)
@@ -69,5 +56,25 @@ public class Day08 {
             }
         }
         return -1;
+    }
+
+    private void resetCounters() {
+        this.counter = 0;
+        this.accumulator = 0;
+    }
+
+    private String buildChangedStatement(String input) {
+        String currentStatement = "";
+        String command = input.split(" ")[0];
+        String value = input.split(" ")[1];
+        switch (command) {
+            case "jmp":
+                currentStatement = "nop " + value;
+                break;
+            case "nop":
+                currentStatement = "jmp " + value;
+                break;
+        }
+        return currentStatement;
     }
 }

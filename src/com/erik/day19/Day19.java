5629d8d9bddc9087a061f09f8a7115424395c7a7
erikwastaken <46598911+erikwastaken@users.noreply.github.com>
2020-12-19 11:22:12 +0100

    Day19 refactoring

--- src/com/erik/day19/Day19.java
+++ src/com/erik/day19/Day19.java
@@ -2,7 +2,6 @@ package com.erik.day19;
 
 import com.erik.AdventDay;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.regex.Matcher;
@@ -70,75 +69,25 @@ public class Day19 implements AdventDay {
     }
 
     public long part2() {
-        // 8 and 11 only occur in rule 0...
+        // 8 and 11 only occur in rule 0;
         // 0 -> 8 11 -> (42 | 42 8) (42 31 | 42 11 31)
         long counter = 0;
-        String rule8 = resolveRule2("8");
-        String rule42 = resolveRule2("42");
-        String rule31 = resolveRule2("31");
+        String rule8 = resolveRule("8");
+        String rule42 = resolveRule("42");
+        String rule31 = resolveRule("31");
 
         // should be enough to include all cases given the input...
         // something like a recursion regex which would allow replacements like 42 11 31 does not seem possible...
-        for (int i=0; i<10; i++) {
-            StringBuilder rule4231 = new StringBuilder();
-            for (int j=0; j<=i; j++) {
-                rule4231.append(rule42);
-            }
-            for (int j=0; j<=i; j++) {
-                rule4231.append(rule31);
-            }
-
-            String r = rule8 + "(" + rule4231.toString() + ")";
-            Pattern pattern = Pattern.compile("^" + r + "$");
+        for (int i=1; i<10; i++) {
+            String r = "^" + rule8 + "+" + "(" + rule42 + "{" + i + "}" + rule31 + "{" + i + "}" + ")" + "$";
+            Pattern pattern = Pattern.compile(r);
 
             for (String message : messages) {
                 Matcher matcher = pattern.matcher(message);
-                if (matcher.find()) {
+                if (matcher.find())
                     counter++;
-                }
             }
         }
         return counter;
-    }
-
-    private String resolveRule2(String key) {
-        StringBuilder exp = new StringBuilder();
-
-        if (key.equals("8")) {
-            exp.append("(");
-            exp.append(resolveRule2("42"));
-            exp.append("+");
-            exp.append(")");
-            return exp.toString();
-        }
-
-        // FIXME: this also matches 42 42 31 which is wrong; the number of 42s and 31s needs to be equal;
-        if (key.equals("11")) {
-            exp.append("(");
-            exp.append(resolveRule2("42"));
-            exp.append("+");
-            exp.append(resolveRule2("31"));
-            exp.append("+");
-            exp.append(")");
-            return exp.toString();
-        }
-
-        String rule = rules.get(key);
-
-        if (rule.contains("a"))
-            return "a";
-        if (rule.contains("b"))
-            return "b";
-
-        exp.append("(");
-        for (String split : rule.split(" ")) {
-            if (split.equals("|")) {
-                exp.append(split);
-                continue;
-            }
-            exp.append(resolveRule2(split));
-        }
-        exp.append(")");
-        return exp.toString();
     }
 }

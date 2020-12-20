da3186e222b06f86c5c9df62f6c8a3108c975e26
erikwastaken <46598911+erikwastaken@users.noreply.github.com>
2020-12-20 08:55:31 +0100

    build Constructor

--- src/com/erik/day20/Day20.java
+++ src/com/erik/day20/Day20.java
@@ -1,10 +1,45 @@
 package com.erik.day20;
 
+import java.util.HashMap;
+
 public class Day20 {
+    private static final String EMPTY = ".";
+    private static final String FULL = "#";
+
+    HashMap<Long, String[][]> images;
     public Day20(String[] input) {
+        images = new HashMap<>();
+        String[][] image = new String[10][10];
+        int lineCount = 0;
+        long uuid = 0;
+
+        for (String line : input) {
+            if (line.length() == 0) {
+                continue;
+            }
+
+            if (line.startsWith("Tile")) {
+                uuid = Long.parseLong(line.substring(5,line.length()-1));
+                continue;
+            }
+
+            for (int i = 0; i < line.length(); i++) {
+                image[lineCount][i] = String.valueOf(line.charAt(i));
+            }
+            lineCount++;
+
+            if (lineCount == 10) {
+                images.put(uuid,image);
+                uuid = 0;
+                image = new String[10][10];
+                lineCount = 0;
+            }
+        }
     }
 
     public long part1() {
+        // assumption: pictures only fit together in one way;
+        
         return -1;
     }
 }

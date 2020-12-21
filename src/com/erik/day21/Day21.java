f52a7e175bae437725e08cd6a102525a67d5af36
erikwastaken <46598911+erikwastaken@users.noreply.github.com>
2020-12-21 15:19:46 +0100

    Part 1 passes acceptance test
    but not with the actual data

---
+++ src/com/erik/day21/Day21.java
@@ -0,0 +1,61 @@
+package com.erik.day21;
+
+import com.erik.AdventDay;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Day21 implements AdventDay {
+    String[] input;
+    public Day21(String[] input) {
+        this.input = input;
+    }
+
+    public long part1() {
+        HashMap<String, HashSet<String>> possibleIngredientsForAllergen = new HashMap<>();
+        HashSet<String> ingredients = new HashSet<>();
+        for (String s : input) {
+            // separate ingredient jibberish from allergens
+            String[] splits = s.split(" \\(contains ");
+            String[] ings = splits[0].split(" ");
+            Collections.addAll(ingredients, ings);
+
+            String[] allergens = splits[1].split(", ");
+            // remove the ) on the last allergen;
+            allergens[allergens.length-1] = allergens[allergens.length-1].substring(0,allergens[allergens.length-1].length()-1);
+
+            for (String a : allergens) {
+                if (possibleIngredientsForAllergen.containsKey(a)) {
+                    possibleIngredientsForAllergen.get(a).retainAll(new HashSet<>(Arrays.asList(ings)));
+                } else {
+                    possibleIngredientsForAllergen.put(a, new HashSet<>(Arrays.asList(ings)));
+                }
+            }
+        }
+
+        HashSet<String> notAllergens = new HashSet<>(ingredients);
+
+        for (String ingredient : ingredients) {
+            for (HashSet<String> value : possibleIngredientsForAllergen.values()) {
+                if (value.contains(ingredient))
+                    notAllergens.remove(ingredient);
+            }
+        }
+
+
+        long counter = 0;
+        for (String ingredient : notAllergens) {
+            for (String line : input) {
+                if (line.contains(ingredient))
+                    counter++;
+            }
+        }
+        return counter;
+    }
+
+    public long part2() {
+        return -1;
+    }
+}
---
+++ src/com/erik/day21/Day21Test.java
@@ -0,0 +1,25 @@
+package com.erik.day21;
+
+import com.erik.InputProvider;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Day21Test {
+    @Test
+    void part1() {
+        String[] input = new String[] {
+                "mxmxvkd kfcds sqjhc nhms (contains dairy, fish)",
+                "trh fvjkl sbzzf mxmxvkd (contains dairy)",
+                "sqjhc fvjkl (contains soy)",
+                "sqjhc mxmxvkd sbzzf (contains fish)"
+        };
+        Day21 cut = new Day21(input);
+        Assertions.assertEquals(5L, cut.part1());
+    }
+
+    @Test
+    void part1WithRealInput() {
+        Day21 cut = new Day21(new InputProvider().get("inputDay21.txt"));
+        Assertions.assertTrue(2670L > cut.part1());
+    }
+}
